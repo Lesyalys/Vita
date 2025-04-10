d1a5895cbf772734a284bc164717a280efffe140
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-11 00:44:51 +0500

    new

---
+++ src/pages/home/home.js
@@ -0,0 +1,26 @@
+ import './home.css'
+
+export function home(element) {
+  element.innerHTML = `
+  <div>
+    <img src="https://i.pinimg.com/736x/8a/39/5c/8a395c1f432ecb52c04c8a023ef4647b.jpg" class ="gameComp" alt="JavaScript logo" />
+
+  </div>
+`
+document.querySelector('#header h1').innerHTML = `
+      <h2>My contact:
+      </h2>
+
+      <ul>
+        <li class="underHearUl"><p id="underHeader">mail:</p></li>
+        <li class="underHearUl"><p id="underHeader"><a href = "nya.olesya@bk.ru" target ="_blank">nya.olesya@bk.ru</a></p></li>
+      </ul>
+
+      <ul>
+        <li class="underHearUl"><p id="underHeader">tg:</p></li>
+        <li class="underHearUl"><p id="underHeader"><a href = "https://t.me/Shadowtav" target ="_blank">@Shadowtav</a></p></li>
+      </ul>
+      `
+  
+}
+
